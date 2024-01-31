0d190135f0fb721b9bee91e892f534ae4037b575
quilde <enseli.nils@gmail.com>
2024-01-31 17:32:46 +0100

    workflow

--- indexdiachronica/diachronica_leptos/dist/diachronica_leptos-ba6d6d0265e164f0.js
+++ indexdiachronica/diachronica_leptos/dist/diachronica_leptos-ba6d6d0265e164f0.js
@@ -785,7 +785,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('diachronica_leptos-cade8ce63ef3882a_bg.wasm', import.meta.url);
+        input = new URL('diachronica_leptos-ba6d6d0265e164f0_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
